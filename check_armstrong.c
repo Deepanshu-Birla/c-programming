8fed3db8a87635a6205085a198ca307c2f2b3c9c
Deepanshu Birla <dipanshubirla083@gmail.com>
2025-10-26 22:33:52 +0530

    checking whether the number is armstong or not

---
+++ check_armstrong.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, originalNum, remainder, result = 0, n = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    originalNum = num;
+
+    // Count number of digits
+    int temp = num;
+    while (temp != 0) {
+        temp /= 10;
+        n++;
+    }
+
+        temp = num;
+    while (temp != 0) {
+        remainder = temp % 10;
+        result += pow(remainder, n);
+        temp /= 10;
+    }
+    if (result == originalNum) {
+        printf("%d is an Armstrong number.\n", originalNum);
+    } else {
+        printf("%d is NOT an Armstrong number.\n", originalNum);
+    }
+
+    return 0;
+}
